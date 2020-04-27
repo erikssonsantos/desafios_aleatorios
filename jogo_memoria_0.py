0f3e5741946fb2ba0c2702efc4f5d4443e338654
Eriksson Santos <erikssonsantos.py@gmail.com>
2020-04-26 23:53:34 -0300

    Update jogo_memoria_0.py

--- jogo_memoria_0.py
+++ jogo_memoria_0.py
@@ -2,62 +2,83 @@ from random import randint
 
 
 class Jogo:
-	
-	def __init__(self):
+    
+    def __init__(self):
         
-		self.loop_do_jogo = True
-		self.numero_de_opcoes = 10
-		self.opcoes_descobertas = []
-		self.cartas = [randint(1, 1000) for i in range(self.numero_de_opcoes // 2)]
-		self.cartas += self.cartas
-		self.carta1 = None
-		self.carta2 = None
-		self.indice_carta1 = None
-		self.indice_carta2 = None
-	
-	def mostrar_carta(self, val, carta):
+        self.loop_do_jogo = True
+        self.numero_de_opcoes = 10
+        self.cartas = [randint(1, 1000) for i in range(self.numero_de_opcoes // 2)]
+        self.cartas += self.cartas
+        self.carta1 = None
+        self.carta2 = None
+        self.indice_carta1 = None
+        self.indice_carta2 = None
+        self.cartas_encontradas = []
         
-		if carta == 1:
-			self.carta1 = self.cartas[val]
-			self.indice_carta1 = val
-		elif carta == 2:
-			self.carta2 = self.cartas[val]
-			self.indice_carta2 = val
-		return self.cartas[val]
-	
-	def comparar_cartas(self, carta1, carta2):
+    def mostrar_carta(self, val, carta):
         
-		if carta1 == carta2:
-			opcoes_descobertas.append(self.indice_carta1)
-			opcoes_descobertas.append(self.indice_carta2)
-			return True
-		else:
-			return False
-			
-	def mostrar_opcoes(self, escolhida1=None, escolhida2=None):
+        if carta == 1:
+            self.carta1 = self.cartas[val]
+            self.indice_carta1 = val
+        elif carta == 2:
+            self.carta2 = self.cartas[val]
+            self.indice_carta2 = val
+        return self.cartas[val]
         
-		for i in range(self.numero_de_opcoes):
-			if escolhida1 and escolhida1 == i:
-				print(self.cartas[escolhida1], end=' ')
-				continue
-			if escolhida2 and escolhida2 == i:
-				print(self.cartas[escolhida2], end=' ')
-				continue
-			print('*', end=' ')
-		print()
-		
-	def fim(self):
-		return
-		
+    def comparar_cartas(self, carta1, carta2):
+        
+        if carta1 == carta2:
+            return True
+        else:
+            return False
+            
+    def mostrar_opcoes(self, escolhida1=None, escolhida2=None):
+        print()
+        for i in range(0, self.numero_de_opcoes):
+            if i in self.cartas_encontradas:
+                print(self.cartas[i], end=' ')
+                continue
+            if escolhida1 == i:
+                print(self.cartas[escolhida1], end=' ')
+                continue
+            elif escolhida2 == i:
+                print(self.cartas[escolhida2], end=' ')
+                continue
+            else:
+                print('*', end=' ')
+        print()
+        
+    def fim(self):
+        print('Jogo finalizado!')
+
 
 jogo = Jogo()
 
-
+vez = 2
+escolhida1 = None
+escolhida2 = None
 while jogo.loop_do_jogo:
     
-	jogo.mostrar_opcoes()
-	escolhido = int(input('Escolha uma opção: '))
-
+    if vez == 1: 
+        vez = 2
+    elif vez == 2:
+        vez = 1
+    
+    jogo.mostrar_opcoes(escolhida1, escolhida2)
+    
+    if vez == 1:
+        escolhida1 = int(input('Escolha a primeira opção: '))
+    if vez == 2:
+        while True:
+            escolhida2 = int(input('Escolha a segunda opção: '))
+            if escolhida2 == escolhida1:
+                print('Você á escolheu esssa opção!')
+            else:
+                break
+        if jogo.comparar_cartas(jogo.cartas[escolhida1], jogo.cartas[escolhida2]):
+            jogo.cartas_encontradas.append(escolhida1)
+            jogo.cartas_encontradas.append(escolhida2)
+            escolhida1 = None
+            escolhida1 = None
 
 jogo.fim()
-
